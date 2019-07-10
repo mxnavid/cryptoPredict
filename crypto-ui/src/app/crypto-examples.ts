528bd62bf637c0abb692578f14e0c4fa767634a8
Tyler Young <tyler210@vt.edu>
2019-07-10 16:09:07 -0400

    Changing to ReactJS boi

--- crypto-ui/src/app/crypto-examples.ts
+++
@@ -1,7 +0,0 @@
-import { Cryptocurrency } from './cryptocurrency';
-
-export const CRYPTOCURRENCIES: Cryptocurrency[] = [
-   { id: 1, title: 'Bitcoin', sentimentScore: 0, changeHour: 1 }, 
-   { id: 2, title: 'Litecoin', sentimentScore: 2, changeHour: 3 }, 
-   { id: 3, title: 'Ethereum', sentimentScore: 4, changeHour: 5 },
-]
--- crypto-ui/src/app/cryptocurrency.ts
+++
@@ -1,6 +0,0 @@
-export class Cryptocurrency {
-   id: number;
-   title: string;
-   sentimentScore: number;
-   changeHour: number;
-}
