10cc60ac40fc12028289f210be7eb116cbf67111
mxnavid <mxn4459@rit.edu>
2019-07-10 14:08:47 -0400

    btc,eth and ltc

--- twitterCrawler/hashtagSearch.py
+++ twitterCrawler/hashtagSearch.py
@@ -4,33 +4,79 @@ import json
 import csv
 import time
 
-api = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET,
-                  ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+api = twitter.Api(CONSUMER_KEY,
+                  CONSUMER_SECRET,
+                  ACCESS_TOKEN_KEY,
+                  ACCESS_TOKEN_SECRET,
+                  sleep_on_rate_limit=True)
+
+
+def demojify(text):
+    return text.encode('ascii', 'ignore').decode('ascii')
+
+
+def bitcoin():
+    with open("bitcoin.csv", 'a') as writer:
+        response = api.GetSearch(
+            raw_query="q=%23bitcoin%2C%20OR%20%23btc&l=en&src=typd")
+        time.sleep(5)
+        y = json.loads(str(response[0]))
+        publishDate = y["created_at"]
+        tweetText = y["text"]
+        tweetText = str(tweetText)
+        tweetText.replace(",", "")
+        tweetText.replace("\n", "")
+        tweetText = demojify(tweetText)
+        accountID = y["id"]
+        writer.write(publishDate + "," + tweetText + "," + "\n")
+        print("bitcoin executed")
+
+
+def ethereum():
+    with open("ethereum.csv", 'a') as writer:
+        response = api.GetSearch(
+            raw_query="q=%23eth%2C%20OR%20%23ethereum&l=en&src=typd")
+        time.sleep(5)
+        y = json.loads(str(response[0]))
+        publishDate = y["created_at"]
+        tweetText = y["text"]
+        tweetText = str(tweetText)
+        tweetText.replace(",", "")
+        tweetText.replace("\n", "")
+        tweetText = demojify(tweetText)
+
+        accountID = y["id"]
+        writer.write(publishDate + "," + tweetText + "," + "\n")
+        print("ethereum got executed")
+
+
+def litecoin():
+    with open("litecoin.csv", 'a') as writer:
+        response = api.GetSearch(
+            raw_query="f=tweets&vertical=news&q=litecoin&l=en&src=typd")
+        time.sleep(2)
+        y = json.loads(str(response[0]))
+        publishDate = y["created_at"]
+        tweetText = y["text"]
+        tweetText = str(tweetText)
+        tweetText.replace(",", "")
+        tweetText.replace("\n", "")
+        accountID = y["id"]
+        writer.write(publishDate + "," + tweetText + "," + str(accountID) +
+                     "\n")
+        print("litecoin got executed")
 
 
 def get_tweets(api):
-    with open("output.csv", 'w+') as writer:
-        for i in range(0, 100000000000000000000000):
-            startTime = time.time()
-            results = api.GetSearch(
-                raw_query="l=en&q=bitcoin&src=typd")
-# TODO %3 = 0 - bitcoin, 1 - eth, 3 - ltc
-            for x in range(0, len(results)):
-                y = json.loads(str(results[x]))
-                date = y["created_at"]
-                status = y["text"]
-                statusTOstr = str(status)
-                statusTOstr = statusTOstr.replace("\n", " ")
-                statusTOstrFINAL = statusTOstr.replace(",", " ")
-                writer.write(str(date) + ' , ' + statusTOstrFINAL + ' \n')
-            # print(parsedJSON["created_at"])
-            time.sleep(5)
-            endTime = time.time()
-            elapsed = endTime-startTime
-            print("I am sleeping for ")
-            print(elapsed)
-
-            print("call number" + str(i))
+    for i in range(0, 21):
+        if i % 3 == 0:
+            bitcoin()
+        elif i % 3 == 1:
+            print()
+            ethereum()
+        else:
+            print()
+            # litecoin()
 
 
 get_tweets(api)
