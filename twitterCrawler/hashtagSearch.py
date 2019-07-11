d9c77167d73ba29d9aacac25d57145bfb4eaa559
mxnavid <mxn4459@rit.edu>
2019-07-11 15:23:36 -0400

    Twitter Formatted Right

--- twitterCrawler/hashtagSearch.py
+++ twitterCrawler/hashtagSearch.py
@@ -2,8 +2,10 @@ import twitter
 from t import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 import json
 import csv
+import re
 import time
 
+sleepTime = 4
 api = twitter.Api(CONSUMER_KEY,
                   CONSUMER_SECRET,
                   ACCESS_TOKEN_KEY,
@@ -15,68 +17,110 @@ def demojify(text):
     return text.encode('ascii', 'ignore').decode('ascii')
 
 
-def bitcoin():
+def cleanTweets(tweet):
+    cleanTweet = re.sub(r'\\x\w+\w', '', tweet)
+    cleanTweet = re.sub(r'(http|#|@)\S+', '', cleanTweet)
+    cleanTweet = re.sub(r',|\n', '', cleanTweet)
+    cleanTweet = demojify(cleanTweet)
+    cleanTweet = "" + cleanTweet + ""
+    # print(cleanTweet)
+    return cleanTweet
+
+
+def bitcoin(previousDate):
     with open("bitcoin.csv", 'a') as writer:
         response = api.GetSearch(
             raw_query="q=%23bitcoin%2C%20OR%20%23btc&l=en&src=typd")
-        time.sleep(5)
+        time.sleep(sleepTime)
         y = json.loads(str(response[0]))
         publishDate = y["created_at"]
-        tweetText = y["text"]
-        tweetText = str(tweetText)
-        tweetText.replace(",", "")
-        tweetText.replace("\n", "")
-        tweetText = demojify(tweetText)
-        accountID = y["id"]
-        writer.write(publishDate + "," + tweetText + "," + "\n")
-        print("bitcoin executed")
+        publishDate = str(publishDate)
+        if publishDate == previousDate:
+            print("Same Date Ignoring It")
+            return 0
+        else:
+            previousDate = publishDate
+
+            tweetText = y["text"]
+            tweetText = str(tweetText)
+            tweetText = cleanTweets(tweetText)
+            accountID = y["id"]
+
+            accountID = "" + str(accountID) + ""
+            writer.write(publishDate + "," + tweetText + "," + accountID +
+                         "\n")
+            print("bitcoin executed")
+            return publishDate
 
 
-def ethereum():
+def ethereum(previousDate):
     with open("ethereum.csv", 'a') as writer:
         response = api.GetSearch(
             raw_query="q=%23eth%2C%20OR%20%23ethereum&l=en&src=typd")
-        time.sleep(5)
+        time.sleep(sleepTime)
         y = json.loads(str(response[0]))
         publishDate = y["created_at"]
-        tweetText = y["text"]
-        tweetText = str(tweetText)
-        tweetText.replace(",", "")
-        tweetText.replace("\n", "")
-        tweetText = demojify(tweetText)
+        publishDate = str(publishDate)
 
-        accountID = y["id"]
-        writer.write(publishDate + "," + tweetText + "," + "\n")
+        if publishDate == previousDate:
+            print("Same Date Ignoring It")
+            return 0
+        else:
+            previousDate = publishDate
+            tweetText = y["text"]
+            tweetText = str(tweetText)
+            tweetText = cleanTweets(tweetText)
+            accountID = y["id"]
+
+            accountID = "" + str(accountID) + ""
+            writer.write(publishDate + "," + tweetText + "," + accountID +
+                         "\n")
+            return publishDate
         print("ethereum got executed")
 
 
-def litecoin():
+def litecoin(previousDate):
     with open("litecoin.csv", 'a') as writer:
         response = api.GetSearch(
             raw_query="f=tweets&vertical=news&q=litecoin&l=en&src=typd")
-        time.sleep(2)
+        time.sleep(sleepTime)
         y = json.loads(str(response[0]))
         publishDate = y["created_at"]
-        tweetText = y["text"]
-        tweetText = str(tweetText)
-        tweetText.replace(",", "")
-        tweetText.replace("\n", "")
-        accountID = y["id"]
-        writer.write(publishDate + "," + tweetText + "," + str(accountID) +
-                     "\n")
+        publishDate = str(publishDate)
+
+        if publishDate == previousDate:
+            print("Same Date Ignoring It")
+            return 0
+        else:
+            previousDate = publishDate
+            tweetText = y["text"]
+            tweetText = str(tweetText)
+            tweetText = cleanTweets(tweetText)
+            accountID = y["id"]
+
+            accountID = "" + str(accountID) + ""
+            writer.write(publishDate + "," + tweetText + "," + accountID +
+                         "\n")
         print("litecoin got executed")
+        return publishDate
 
 
 def get_tweets(api):
-    for i in range(0, 21):
+    previousDateBTC = ""
+    previousDateETH = ""
+    previousDateLTC = ""
+    i = 0
+    while True:
         if i % 3 == 0:
-            bitcoin()
+            retrieveBTC = bitcoin(previousDateBTC)
+            previousDateBTC = retrieveBTC
         elif i % 3 == 1:
-            print()
-            ethereum()
+            retrieveETH = ethereum(previousDateETH)
+            previousDateETH = retrieveETH
         else:
-            print()
-            # litecoin()
+            retrieveLTC = litecoin(previousDateLTC)
+            previousDateLTC = retrieveLTC
+        i += 1
 
 
 get_tweets(api)
