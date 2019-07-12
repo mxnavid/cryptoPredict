ad0102b54d26d74e355ef8a9c3291b0f44e6ddd4
Tyler Young <tyler210@vt.edu>
2019-07-12 11:35:34 -0400

    idk

--- ui/src/App.js
+++ ui/src/App.js
@@ -19,7 +19,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
 import { IconButton } from "@material-ui/core";
 
-import Grid from '@material-ui/core/Grid';
+
+import { MemoryRouter as Router } from 'react-router';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+
+// import { Dashboard } from "./components/Dashboard";
+// import { Portfolio } from "./components/Portfolio";
+// import { Bitcoin } from "./components/Bitcoin";
+
+import Grid from "@material-ui/core/Grid";
 
 const drawerWidth = 240;
 
@@ -92,109 +101,117 @@ function App() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
 
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Cryptowatch
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Cryptowatch
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                inputProps={{ "aria-label": "Search" }}
+              />
             </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ "aria-label": "Search" }}
-            />
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List
-          subheader={
-            <ListSubheader component="div" id="nested-list-subheader">
-              Views
-            </ListSubheader>
-          }
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper
+          }}
         >
-          {["Dashboard", "Portfolio"].map(
-            (text, index) => (
+          <div className={classes.toolbar} />
+          <List
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Views
+              </ListSubheader>
+            }
+          >
+            {["Dashboard", "Portfolio"].map((text, index) => (
               <ListItem button key={text}>
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItem>
-            )
-          )}
-        </List>
+            ))}
+          </List>
 
-        <Divider />
+          <Divider />
 
-        <List
-          subheader={
-            <ListSubheader component="div" id="nested-list-subheader">
-              Cryptocurrencies
-            </ListSubheader>
-          }
-          Î
-        >
-          {["Bitcoin", "Litecoin", "Ethereum"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+          <List
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Cryptocurrencies
+              </ListSubheader>
+            }
+            Î
+          >
+            {["Bitcoin", "Litecoin", "Ethereum"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
 
-        <Divider />
+          <Divider />
 
-        <List
-          subheader={
-            <ListSubheader component="div" id="nested-list-subheader">
-              Tools
-            </ListSubheader>
-          }
-          
-        >
-          {["Sentiment Analysis", "Charts Galore", "Playground (To Be Removed)", "Settings"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+          <List
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Tools
+              </ListSubheader>
+            }
+          >
+            {[
+              "Sentiment Analysis",
+              "Charts Galore",
+              "Playground (To Be Removed)",
+              "Settings"
+            ].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
 
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <p>Hello</p>
+          {/* <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} /> */}
+ 
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <p>Hello</p>
+            </Grid>
+            <Grid item xs={6}>
+              <p>there</p>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <p>there</p>
-          </Grid>
-        </Grid>
-      </main>
-    </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
