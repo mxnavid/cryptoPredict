d3559f10895572a5d046a2c067af551718597ae6
Tyler Young <tyler210@vt.edu>
2019-07-15 15:46:55 -0400

    idk

--- ui/src/Litecoin_hourly_output.js
+++ ui/src/Litecoin_hourly_output.js
@@ -1,1 +1,48 @@
-module.exports = { hourly_data : [{"Time":"2019-07-15 03","Open":88.13,"Close":87.63,"High":88.22,"Low":85.82,"VolumeCoin":43387.71,"VolumeUSD":3776505.6299999999,"Polarity":0.0808305885,"Subjectivity":0.1948660518},{"Time":"2019-07-15 04","Open":87.63,"Close":89.25,"High":89.38,"Low":87.63,"VolumeCoin":12749.77,"VolumeUSD":1130561.8500000001,"Polarity":0.0492447977,"Subjectivity":0.1785662111},{"Time":"2019-07-15 05","Open":89.25,"Close":87.83,"High":89.28,"Low":87.44,"VolumeCoin":18296.13,"VolumeUSD":1610502.9399999999,"Polarity":0.0540785399,"Subjectivity":0.199700918},{"Time":"2019-07-15 13","Open":91.54,"Close":92.55,"High":92.92,"Low":90.65,"VolumeCoin":38015.44,"VolumeUSD":3496210.0600000001,"Polarity":0.025,"Subjectivity":0.075}]}
+module.exports = {
+  hourly_data: [
+    {
+      Time: "2019-07-15 03",
+      Open: 88.13,
+      Close: 87.63,
+      High: 88.22,
+      Low: 85.82,
+      VolumeCoin: 43387.71,
+      VolumeUSD: 3776505.6299999999,
+      Polarity: 0.0808305885,
+      Subjectivity: 0.1948660518
+    },
+    {
+      Time: "2019-07-15 04",
+      Open: 87.63,
+      Close: 89.25,
+      High: 89.38,
+      Low: 87.63,
+      VolumeCoin: 12749.77,
+      VolumeUSD: 1130561.8500000001,
+      Polarity: 0.0492447977,
+      Subjectivity: 0.1785662111
+    },
+    {
+      Time: "2019-07-15 05",
+      Open: 89.25,
+      Close: 87.83,
+      High: 89.28,
+      Low: 87.44,
+      VolumeCoin: 18296.13,
+      VolumeUSD: 1610502.9399999999,
+      Polarity: 0.0540785399,
+      Subjectivity: 0.199700918
+    },
+    {
+      Time: "2019-07-15 13",
+      Open: 91.54,
+      Close: 92.55,
+      High: 92.92,
+      Low: 90.65,
+      VolumeCoin: 38015.44,
+      VolumeUSD: 3496210.0600000001,
+      Polarity: 0.025,
+      Subjectivity: 0.075
+    }
+  ]
+};
