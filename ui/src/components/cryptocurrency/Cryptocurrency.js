215b1b0e86f679174a4784b233101c527a3335bf
Tyler Young <tyler210@vt.edu>
2019-07-16 10:01:03 -0400

    Cryptocurrency additions

--- ui/src/components/cryptocurrency/Cryptocurrency.js
+++ ui/src/components/cryptocurrency/Cryptocurrency.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import Card2 from "../layout/Card2";
+// {props.match.params.coin}
 const Cryptocurrency = props => {
   return (
     <div className="section has-background-light ">
-      <div className="columns is-multiline is-mobile">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title title">{props.match.params.coin}</p>
-          </header>
-        </div>
-
-        {/* <h1 className="title">{props.match.params.coin}</h1> */}
+      <div className="columns">
+        <h1 className="title column" style={{ fontSize: "48px" }}>
+          {props.match.params.coin}
+        </h1>
+      </div>
+      <div className="columns is-mobile is-multiline">
+         <Card2 />
+         <Card2 />
+         <Card2 />
+         <Card2 />
       </div>
     </div>
   );
