16bc4a8a7ff268cb6f393ca47e6d12b1a5db62d4
mxnavid <mxn4459@rit.edu>
2019-07-23 10:24:15 -0400

    NewsCrawler Finalized

--- newsCrawler/crawler.py
+++ newsCrawler/crawler.py
@@ -27,45 +27,67 @@ def api_col(fromDate, toDate, coin):
                                             cnbc, cnn, daily-mail, the-economist, techcrunch,\
                                             the-huffington-post, the-wall-street-journal, the-washington-post, \
                                              usa-today, time, wired',
-        from_param='2019-07-30',
-        to='2019-07-01',
+        from_param=str(fromDate),
+        to=str(toDate),
         language='en',
         sort_by='relevancy',
         page=2)
-    return all_articles
+    dump = json.dumps(all_articles)
+    y = json.loads(dump)
+    return y
 
 
-def writer():
-    # dump = json.dumps(all_articles)
-    # y = json.loads(dump)
+def writer(coinName, y):
+    articleNumber = y["totalResults"]
+    lele = len(y["articles"])
+    print(lele)
+    if coinName == 'bitcoin':
+        fileName = 'btc.csv'
+    elif coinName == 'ethereum':
+        fileName = 'eth.csv'
+    else:
+        fileName = 'ltc.csv'
+    with open(fileName, 'a') as writer:
+        for x in range(0, lele):
+            r = y["articles"][x]
+            dumper = json.dumps(r)
+            parser = json.loads(dumper)
+            title = parser["title"]
+            title = str(title)
+            title = title.replace(",", "")
+            title = title.replace("\n", "")
+            description = parser["description"]
+            description = str(description)
+            description = description.replace(",", "")
+            description = description.replace("\n", "")
+            publishTime = parser["publishedAt"]
+            publisher = parser["source"]["name"]
+            writer.write(publishTime + "," + title + "," + description + "," +
+                         publisher + "\n")
+
+
+def processor():
+
+    datePrev = datetime.datetime(2019, 7, 18).date()
+    dateLater = datetime.datetime(2019, 7, 19).date()
+
     for i in range(0, 10):
-        date = datetime.datetime(2019, 7, 1)
-        print(date)
+        datePrev += datetime.timedelta(days=1)
+        dateLater += datetime.timedelta(days=1)
+        for x in range(0, 2):
+            if x == 0:
+                responseBTC = api_col(datePrev, dateLater, 'bitcoin')
+                writer('bitcoin', responseBTC)
+                print("BTC executed")
+            elif x == 1:
+                responseETH = api_col(datePrev, dateLater, 'ethereum')
+                writer('ethereum', responseETH)
+                print("ETH executed")
 
-        # with open("ee.json", 'w+') as e:
-        #     e.write(str(y))
-        # articleNumber = y["totalResults"]
-        # lele = len(y["articles"])
-        # print(lele)
-        # with open("output.csv", 'a') as writer:
-        #     for x in range(0, lele):
-        #         r = y["articles"][x]
-        #         dumper = json.dumps(r)
-        #         parser = json.loads(dumper)
-        #         title = parser["title"]
-        #         title = str(title)
-        #         title = title.replace(",", "")
-        #         title = title.replace("\n", "")
-        #         description = parser["description"]
-        #         description = str(description)
-        #         description = description.replace(",", "")
-        #         description = description.replace("\n", "")
-        #         publishTime = parser["publishedAt"]
-        #         publisher = parser["source"]["name"]
-        #         writer.write(publishTime + "," + title + "," + description + "," +
-        #                     publisher + "\n")
+            else:
+                responseLTC = api_col(datePrev, dateLater, 'litecoin')
+                writer('litecoin', responseLTC)
+                print("LTC Executed")
 
 
-# print(all_articles)
-
-writer()
+processor()
