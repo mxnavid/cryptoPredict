db795b2261265fe26646085ac8cdbf001cef019e
Tyler Young <tyler210@vt.edu>
2019-07-24 15:33:15 -0400

    Idk what I'm doing

--- ui/src/components/sentiment/Sentiment.js
+++ ui/src/components/sentiment/Sentiment.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react'
-
-
-
+import React, { Component } from "react";
 
 export default class Sentiment extends Component {
-
-componentDidMount() {
-   fetch('http://127.0.0.1:8080/needScore', {
-      method: 'POST',
-      mode: 'no-cors',
+  componentDidMount() {
+     let requestThing = {"textFromUI": "I hate trump"};
+    fetch("http://127.0.0.1:8080/needScore", {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(requestThing),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      //   'Access-Control-Allow-Credentials': true
-      },
-      body: JSON.stringify({
-        "textFromUI": "I hate trump",
-      })})
-    .then((response) => response.json())
-    .then((responseJson) => {
-
-      console.log(responseJson)
+        'Content-Type': 'application/json'
+        //   'Access-Control-Allow-Credentials': true
+      }
     })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-   render() {
-      return (
-         <div>
-            <p>Sentiment Yo</p>
-         </div>
-      )
-   }
+  render() {
+    return (
+      <div>
+        <p>Sentiment Yo</p>
+      </div>
+    );
+  }
 }
