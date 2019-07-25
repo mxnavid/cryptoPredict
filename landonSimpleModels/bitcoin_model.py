3c12f305ccd6d9a6a6cfd1fa643ac296430608ac
smithlandon <smithlandon98@gmail.com>
2019-07-25 11:45:35 -0400

    giving tyler y more stuff to play with

--- landonSimpleModels/bitcoin_model.py
+++ landonSimpleModels/bitcoin_model.py
@@ -133,11 +133,21 @@ print(df['market_cu_return'][-1:])
 print(df)
 #people look at this, see what kind of data you want to play with here
 df.reset_index(level=0, inplace=True)
+
+my_score=cls.score(X,y)
+df.insert(18, 'Score', my_score)
+df.insert(19, 'Sharpe', Sharpe)
+
+
+
 path = os.path.dirname(os.path.abspath(__file__))
 util.writeDFtoCSV(df, os.path.join(path, 'Bitcoin_model_output.csv'))
-
 outputFileName = '../ui/src/scraped/bitcoin/Bitcoin_model_output.js'
 with open(outputFileName, 'w') as f:
     f.write("module.exports = { model_data : ")
     f.write(df.to_json(orient='records'))
     f.write("}")
+
+print(df)
+#last #some number correct/wrong ratio
+#predicted
