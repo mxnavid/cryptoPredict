dfff94d05aee5e65d9164b523f5654fb96fe742b
Tyler Young <tyler210@vt.edu>
2019-07-29 16:20:21 -0400

    getting rid of old stuff

--- ui/src/scraped/litecoin/Litecoin_daily_output.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hourly_data: [
-    { Time: "2019-07-15", Views: 552, Polarity: null, Subjectivity: null },
-    { Time: "2019-07-16", Views: 548, Polarity: null, Subjectivity: null },
-    { Time: "2019-07-17", Views: 522, Polarity: null, Subjectivity: null }
-  ]
-};
