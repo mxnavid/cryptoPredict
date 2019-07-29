111f4b07bf84623c3a62bd18ff78dd853bbb3cb3
Tyler Wenger <trw34@duke.edu>
2019-07-29 15:48:28 -0400

    Uses parallel processing for data + models

--- runner.py
+++ runner.py
@@ -10,6 +10,7 @@ from landonSimpleModels.bitcoin_model import bitcoinModel
 from landonSimpleModels.litecoin_model import litecoinModel
 from landonSimpleModels.ethereum_model import ethereumModel
 import pandas as pd
+import numpy as np
 import ray
 
 @ray.remote
@@ -21,6 +22,10 @@ def bitcoin2(THIS_FOLDER):
 def bitcoin3(bitcoin, THIS_FOLDER, sp500, usdeuro):
     bitcoin.dict['5min'] = pd.merge(bitcoin.dict['5min'], sp500, on='Time', sort=False, how='outer')
     bitcoin.dict['5min'] = pd.merge(bitcoin.dict['5min'], usdeuro, on='Time', sort=False, how='outer')
+    bitcoin.dict['5min'] = bitcoin.dict['5min'].sort_values(bitcoin.dict['5min'].columns[0], ascending=True)
+    bitcoin.dict['5min'].replace('', np.nan, inplace=True)
+    bitcoin.dict['5min'] = bitcoin.dict['5min'].fillna(method='ffill')
+    bitcoin.dict['5min'] = bitcoin.dict['5min'].tail(2000)
     util.writeFiles(bitcoin, THIS_FOLDER)
     bitcoinModel(os.path.join(THIS_FOLDER, 'landonSimpleModels/Bitcoin_5min_output.csv'),
                  os.path.join(THIS_FOLDER, 'ui/src/scraped/bitcoin/Bitcoin_model_output.js'))
@@ -35,6 +40,10 @@ def litecoin3(litecoin, THIS_FOLDER, sp500, usdeuro):
     litecoin = CryptoCrawler('Litecoin', 'LTC', os.path.join(THIS_FOLDER, 'data/litecoin.csv'), t2)
     litecoin.dict['5min'] = pd.merge(litecoin.dict['5min'], sp500, on='Time', sort=False, how='outer')
     litecoin.dict['5min'] = pd.merge(litecoin.dict['5min'], usdeuro, on='Time', sort=False, how='outer')
+    litecoin.dict['5min'] = litecoin.dict['5min'].sort_values(litecoin.dict['5min'].columns[0], ascending=True)
+    litecoin.dict['5min'].replace('', np.nan, inplace=True)
+    litecoin.dict['5min'] = litecoin.dict['5min'].fillna(method='ffill')
+    litecoin.dict['5min'] = litecoin.dict['5min'].tail(2000)
     util.writeFiles(litecoin, THIS_FOLDER)
     litecoinModel(os.path.join(THIS_FOLDER, 'landonSimpleModels/Litecoin_5min_output.csv'),
                   os.path.join(THIS_FOLDER, 'ui/src/scraped/litecoin/Litecoin_model_output.js'))
@@ -48,6 +57,10 @@ def ethereum2(THIS_FOLDER):
 def ethereum3(ethereum, THIS_FOLDER, sp500, usdeuro):
     ethereum.dict['5min'] = pd.merge(ethereum.dict['5min'], sp500, on='Time', sort=False, how='outer')
     ethereum.dict['5min'] = pd.merge(ethereum.dict['5min'], usdeuro, on='Time', sort=False, how='outer')
+    ethereum.dict['5min'] = ethereum.dict['5min'].sort_values(ethereum.dict['5min'].columns[0], ascending=True)
+    ethereum.dict['5min'].replace('', np.nan, inplace=True)
+    ethereum.dict['5min'] = ethereum.dict['5min'].fillna(method='ffill')
+    ethereum.dict['5min'] = ethereum.dict['5min'].tail(2000)
     util.writeFiles(ethereum, THIS_FOLDER)
     ethereumModel(os.path.join(THIS_FOLDER, 'landonSimpleModels/Ethereum_5min_output.csv'),
                   os.path.join(THIS_FOLDER, 'ui/src/scraped/ethereum/Ethereum_model_output.js'))
@@ -56,6 +69,7 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
 prevTime = datetime.datetime.now() + timedelta(minutes=-5)
+ray.init()
 while True:
     if (datetime.datetime.now() > prevTime + timedelta(minutes=5)):
         prevTime = datetime.datetime.now()
@@ -66,7 +80,6 @@ while True:
         sts = os.waitpid(p.pid, 0)
 
         print('download done')
-        ray.init()
         bitcoin = bitcoin2.remote(THIS_FOLDER)
         ethereum = ethereum2.remote(THIS_FOLDER)
         litecoin = litecoin2.remote(THIS_FOLDER)
