3717367487b85cc57c7729651d5ab684fd4a0c46
Tyler Young <tyler210@vt.edu>
2019-07-29 02:08:13 -0400

    Dashboard changes

--- ui/src/components/sentiment/Sentiment.js
+++ ui/src/components/sentiment/Sentiment.js
@@ -41,26 +41,6 @@ export default class Sentiment extends Component {
       this.setState({ inputText: text.target.value.substr(0, 100) });
    }
 
-   
-//   componentDidMount() {
-//      let requestThing = {"textFromUI": "I hate trump"};
-//     fetch("http://127.0.0.1:8080/needScore", {
-//       method: "POST",
-//       mode: "no-cors",
-//       body: JSON.stringify(requestThing),
-//       headers: {
-//         'Content-Type': 'application/json'
-//         //   'Access-Control-Allow-Credentials': true
-//       }
-//     })
-//       .then(response => response.json())
-//       .then(responseJson => {
-//         console.log(responseJson);
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   }
 
    render() {
       return (
